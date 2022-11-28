5116216666f003a0d52b6ab6df51142e94333b86
chicodespons <pieterdedycker@gmail.com>
2022-11-28 16:07:24 +0100

    first commit

---
+++ oorder/src/main/java/com/swichfully/chicodespons/oorder/OorderApplication.java
@@ -0,0 +1,13 @@
+package com.swichfully.chicodespons.oorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OorderApplication.class, args);
+	}
+
+}
---
+++ oorder/src/test/java/com/swichfully/chicodespons/oorder/OorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swichfully.chicodespons.oorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
