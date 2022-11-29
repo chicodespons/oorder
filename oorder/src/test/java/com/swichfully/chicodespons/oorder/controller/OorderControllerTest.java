2c05dbfe1384ae5b016205c5941d60bc6f69b2a4
chicodespons <pieterdedycker@gmail.com>
2022-11-29 10:23:01 +0100

    test

---
+++ oorder/src/test/java/com/swichfully/chicodespons/oorder/controller/OorderControllerTest.java
@@ -0,0 +1,12 @@
+package com.swichfully.chicodespons.oorder.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OorderControllerTest {
+
+    @Test
+    void helloWorld() {
+    }
+}
