abfbdfc23e81ce6be3c0377cdf7b9f59400d456c
chicodespons <pieterdedycker@gmail.com>
2022-11-30 17:04:57 +0100

    UserStory3: addOrder met testen(maar nog niet inorde)

--- oorder/src/main/java/com/swichfully/chicodespons/oorder/controller/OrderController.java
+++ oorder/src/main/java/com/swichfully/chicodespons/oorder/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.swichfully.chicodespons.oorder.objects.Order;
 import com.swichfully.chicodespons.oorder.security.Feature;
 import com.swichfully.chicodespons.oorder.security.SecurityService;
 import com.swichfully.chicodespons.oorder.service.OrderService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,7 @@ public class OrderController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
     public Order orderItems(@RequestHeader String authorization, @RequestBody OrderDto orderDto){
         securityService.validateAuthorization(authorization, Feature.ORDER_ITEMS);
         return orderService.orderItems(orderDto, authorization);
